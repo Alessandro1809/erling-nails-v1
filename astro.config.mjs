f770b1b0188b043d56e69c5ddf8e0ea48a50efe8
Alessandro1809 <dalessandro18dg@gmail.com>
2025-05-28 14:57:02 -0600

    agregando server addapter #7

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,6 +15,9 @@ export default defineConfig({
     plugins: [tailwindcss()],
     build: {
       target: 'esnext'
+    },
+    ssr: {
+      target: 'webworker'
     }
   },
   integrations: [react()],
