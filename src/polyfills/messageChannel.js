767e025e0f507d63b86225cb131bf8b0c12d500b
Alessandro1809 <dalessandro18dg@gmail.com>
2025-05-28 15:41:46 -0600

    deploy 3

--- src/polyfills/messageChannel.js
+++ src/polyfills/messageChannel.js
@@ -1,34 +1,69 @@
-// Polyfill para MessageChannel
-class MessagePort {
-  constructor() {
-    this.onmessage = null;
-    this._otherPort = null;
-  }
-
-  postMessage(message) {
-    if (this._otherPort && this._otherPort.onmessage) {
-      const event = { data: message };
-      setTimeout(() => {
-        this._otherPort.onmessage(event);
-      }, 0);
-    }
-  }
-
-  start() {}
-  close() {}
-}
-
+// Polyfill para MessageChannel en Cloudflare Workers
 if (typeof MessageChannel === 'undefined') {
+  class MessagePort {
+    constructor() {
+      this.onmessage = null;
+      this._listeners = new Set();
+    }
+
+    addEventListener(type, listener) {
+      if (type === 'message') {
+        this._listeners.add(listener);
+      }
+    }
+
+    removeEventListener(type, listener) {
+      if (type === 'message') {
+        this._listeners.delete(listener);
+      }
+    }
+
+    dispatchEvent(event) {
+      if (this.onmessage) {
+        this.onmessage(event);
+      }
+      for (const listener of this._listeners) {
+        listener(event);
+      }
+      return true;
+    }
+
+    postMessage(message) {
+      if (this._otherPort) {
+        const event = new MessageEvent('message', { data: message });
+        setTimeout(() => {
+          this._otherPort.dispatchEvent(event);
+        }, 0);
+      }
+    }
+
+    start() {}
+    close() {}
+  }
+
   globalThis.MessageChannel = class MessageChannel {
     constructor() {
-      this.port1 = new MessagePort();
-      this.port2 = new MessagePort();
+      const port1 = new MessagePort();
+      const port2 = new MessagePort();
       
-      // Conectar los puertos
-      this.port1._otherPort = this.port2;
-      this.port2._otherPort = this.port1;
+      port1._otherPort = port2;
+      port2._otherPort = port1;
+      
+      this.port1 = port1;
+      this.port2 = port2;
     }
   };
+
+  if (typeof MessageEvent === 'undefined') {
+    globalThis.MessageEvent = class MessageEvent {
+      constructor(type, options = {}) {
+        this.type = type;
+        this.data = options.data;
+        this.target = null;
+        this.currentTarget = null;
+      }
+    };
+  }
 }
 
 export default MessageChannel; 
