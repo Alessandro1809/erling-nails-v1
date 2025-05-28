49f62eadc3b5c70dd7de539e7fce920fdff6b12d
Alessandro1809 <dalessandro18dg@gmail.com>
2025-05-28 14:05:10 -0600

    agregando server addapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,23 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   integrations: [react()],
+
   image: {
     service: {
       entrypoint: 'astro/assets/services/noop'
     }
-  }
+  },
+
+  adapter: cloudflare()
 });
